16cf2deec4b960b0d019658183f1954066a688ff
Islom-Inomjonov <90414839+Islom-Inomjonov@users.noreply.github.com>
2021-09-15 09:40:23 +0300

    Add files via upload
    Getting input with examples

---
+++ Favorite.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+favorite_color = input("What is your favorite color? ")
+favorite_hobby = input("What is your favorite hobby? ")
+print(name + " likes " + favorite_color + " and " + favorite_hobby)
